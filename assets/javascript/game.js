5b508129aab781cf97119d7b8ebf1952a2572c40
Sammy Ahmed <brsammyahmed@gmail.com>
2017-04-14 07:28:30 -0700

    completed basic html/css skeleton and linked JS document. Added pseudocode to js document as a guideline.

---
+++ assets/javascript/game.js
@@ -0,0 +1,13 @@
+// 1. create var/function that initializes the game
+// 2. a random # should appear between 19-120, which will be the 
+// goal of the game.
+// 3. when the player clicks on a crystal, a random value between 
+// 1-12 will be assigned to each crystal. The value of the crystal
+// will be displayed in the "your score div." The score will increase
+// upon each click, which will be the only indication of the value assigned 
+// to that crystal.
+// 4. The player must reach the goal # via his clicks of 
+// strategic gems. If he matches the goal # the player wins.
+// If the player goes over he loses.
+// The wins and losses will increment upon each game completion
+// and the game will restart with new values.
